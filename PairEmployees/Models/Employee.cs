09cf0963618eb7791d239c4c08e7aaba584a9d69
Mehmed Yusuf <mehmed.yf@gmail.com>
2019-02-03 09:35:25 +0200

    first commit

---
+++ PairEmployees/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairEmployees.Models
+{
+    class Employee
+    {
+        public int EmpId { get; private set; }
+        public int ProjectId { get; private set; }
+        public DateTime DateFrom { get; private set; }
+        public DateTime DateTo { get; private set; }
+        public double Duration { get; private set; }
+
+        public Employee(string empId, string projectId, string dateFrom, string dateTo)
+        {
+            this.EmpId = Convert.ToInt32(empId);
+            this.ProjectId = Convert.ToInt32(projectId);
+            this.DateFrom = Convert.ToDateTime(dateFrom);
+            this.DateTo = (dateTo.ToLower() == "null" ? DateTime.Now.Date : Convert.ToDateTime(dateTo));
+            this.Duration = (this.DateTo - this.DateFrom).TotalDays;
+        }
+    }
+}
---
+++ PairEmployees/Models/EmployeesPair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairEmployees.Models
+{
+    class EmployeesPair
+    {
+        public int projectId { get; set; }
+        public int employeeOne { get; set; }
+        public int employeeTwo { get; set; }
+        public int teamDays { get; set; }
+
+        public EmployeesPair(int projectId, int empOne, int empTwo, int teamDays)
+        {
+            this.projectId = projectId;
+            this.employeeOne = empOne;
+            this.employeeTwo = empTwo;
+            this.teamDays = teamDays;
+        }
+    }
+}
